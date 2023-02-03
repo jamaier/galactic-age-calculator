9f481d47dc7f6b04b2dd7f36fdd1f3a3f9c62ee8
Jacob Maier <jacobamaier@gmail.com>
2023-02-03 11:53:11 -0800

    Good pass on yearsSince.

--- src/js/main.js
+++ src/js/main.js
@@ -26,6 +26,26 @@ export default class GalacticAge {
   }
 
   yearsUntil() {
+    let futureYears = this.futureAge - this.currentAge;
+    let futureMurcury = Math.round(futureYears / 0.24);
+    this.mercurianAge += futureMurcury;
+    let futureVenus = Math.round(futureYears / 0.62);
+    this.venusianAge += futureVenus;
+    let futureMars = Math.round(futureYears / 1.88);
+    this.martianAge += futureMars;
+    let futureJupiter = Math.round(futureYears / 11.86);
+    this.jupiterianAge += futureJupiter;
+  }
 
+  yearsSince() {
+    let pastYears = this.currentAge - this.pastAge;
+    let pastMurcury = Math.round(pastYears / 0.24);
+    this.mercurianAge += pastMurcury;
+    let pastVenus = Math.round(pastYears / 0.62);
+    this.venusianAge += pastVenus;
+    let pastMars = Math.round(pastYears / 1.88);
+    this.martianAge += pastMars;
+    let pastJupiter = Math.round(pastYears / 11.86);
+    this.jupiterianAge += pastJupiter;
   }
 }
