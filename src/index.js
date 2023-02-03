97357287a946185217571ea770043e06675780c1
Jacob Maier <jacobamaier@gmail.com>
2023-02-03 14:37:15 -0800

    WIP: Add buttons for past and current age; brainstorming on UI logic.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
-// import { GalacticAge } from "./../src/js/main";
+import { GalacticAge } from "./../src/js/main";
 
+const handleForm = () => {
+  const currentAge = document.getElementById("current-age").value;
+  const pastAge = document.getElementById("past-age").value;
+  const futureAge = document.getElementById("future-age").value;
+  const newAge = new GalacticAge();
+
+}
+
+
+window.addEventListener("load", () => {
+  const age = new GalacticAge();
+  const form = document.querySelector("form");
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    handleForm();
+  })
+  
+})
 
