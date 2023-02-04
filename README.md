dff8154a798b67cd1a2da1592cb0b02ebdb38fd9
Jacob Maier <jacobamaier@gmail.com>
2023-02-03 16:44:13 -0800

    Update README. Remove bootstrap style in html and re-work some UI logic.

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# _{Application Name}_
+# _Space Age Calculator_
 
 #### By _Jacob Maier_
 
-#### _{Brief description of application}_
+#### _Takes age from user and returns the age you would be relative to that planet._
 
 ## Technologies Used
 
@@ -17,20 +17,17 @@
 
 ## Description
 
-_{}_
+_This web page is supposed to take a current, past and future age from the user to get a return of the current age relative to that planet as well as tell you how much time has passed or will need to pass until the other inputted ages._
 
 ## Setup/Installation Requirements
 
-* _This is a great place_
-* _to list setup instructions_
-* _in a simple_
-* _easy-to-understand_
-* _format_
-
+* _Clone this repo to your local machine_
+* _Run "npm install" to get appropriate packages_
+* _Run "npm start" to open src files into web browser_
 
 ## Known Bugs
 
-* _Any known issues_
+* _No display to user at this time_
 
 ## License
 
--- src/index.js
+++ src/index.js
@@ -7,26 +7,19 @@ const handleForm = () => {
   const currentAge = document.getElementById("current-age").value;
   const pastAge = document.getElementById("past-age").value;
   const futureAge = document.getElementById("future-age").value;
-  const newAge = new GalacticAge(pastAge, currentAge, futureAge);
+  const newAge = new GalacticAge(currentAge);
+  const mercPast = document.getElementById("mercury-past-age");
   newAge.mercury();
-  newAge.venus();
-  newAge.mars();
-  newAge.jupiter();
-  newAge.yearsSince();
-  newAge.yearsUntil();
-  document.getElementById("mercury").innerHTML = `${newAge.mercurianAge}`;
-  document.getElementById("venus").innerHTML = `${newAge.venusianAge}`;
-  document.getElementById("mars").innerHTML = `${newAge.martianAge}`;
-  document.getElementById("jupiter").innerHTML = `${newAge.jupiterianAge}`;
+
+  document.getElementById("mercury-currentAge").innerHTML = `${newAge.mercurianAge}`;
+
 };
 
-
-
 window.addEventListener("load", () => {
-  const age = new GalacticAge();
   const form = document.querySelector("form");
   form.addEventListener("submit", (event) => {
     event.preventDefault();
     handleForm();
   });
 });
+
