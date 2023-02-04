0e65ec97e6cb069c1babed6dfe114349b619786a
Jacob Maier <jacobamaier@gmail.com>
2023-02-03 16:01:26 -0800

    WIP: Add UI update. still working through it.

--- src/index.js
+++ src/index.js
@@ -7,9 +7,19 @@ const handleForm = () => {
   const currentAge = document.getElementById("current-age").value;
   const pastAge = document.getElementById("past-age").value;
   const futureAge = document.getElementById("future-age").value;
-  const newAge = new GalacticAge();
+  const newAge = new GalacticAge(pastAge, currentAge, futureAge);
+  newAge.mercury();
+  newAge.venus();
+  newAge.mars();
+  newAge.jupiter();
+  newAge.yearsSince();
+  newAge.yearsUntil();
+  document.getElementById("mercury").innerHTML = `${newAge.mercurianAge}`;
+  document.getElementById("venus").innerHTML = `${newAge.venusianAge}`;
+  document.getElementById("mars").innerHTML = `${newAge.martianAge}`;
+  document.getElementById("jupiter").innerHTML = `${newAge.jupiterianAge}`;
+};
 
-}
 
 
 window.addEventListener("load", () => {
@@ -18,7 +28,5 @@ window.addEventListener("load", () => {
   form.addEventListener("submit", (event) => {
     event.preventDefault();
     handleForm();
-  })
-  
-})
-
+  });
+});
